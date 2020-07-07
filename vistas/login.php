268b5e425df5fe89fc1febbd358456d8a03fb28f
NataliaSantiago <61957975+NataliaSantiago@users.noreply.github.com>
2020-07-06 23:36:53 -0500

    Update login.php

--- vistas/login.php
+++ vistas/login.php
@@ -11,14 +11,12 @@
 <body background ="3409778.jpg">
     
     <form action="" method="POST">
-       <?php
-
-       ?>
+      
         <h2>Iniciar sesión</h2>
         <p>Nombre de usuario: <br>
-        <input type="text" name="username"></p>
+        <input type="text" name="username" value="luis"></p>
         <p>Password: <br>
-        <input type="password" name="password"></p>
+        <input type="password" name="password" value="rios"></p>
         <p class="center"><input type="submit" value="Iniciar Sesión"></p>
     </form>
 </body>
