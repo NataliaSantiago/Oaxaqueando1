528ec9915ec0da53c4daea29ed5c2f98440ee51c
NataliaSantiago <61957975+NataliaSantiago@users.noreply.github.com>
2020-07-06 23:09:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,20 +6,20 @@ include_once 'includes/user_session.php';
 $userSession = new UserSession();
 $user = new User();
 
-if(isset($_SESSION['user'])){
+//if(isset($_SESSION['user'])){
     //echo "Hay sesión";
-    $user->setUser($userSession->getCurrentUser());
-    include_once 'vistas/home.php';
-}else if(isset($_POST['username']) && isset($_POST['password'])){
-    //echo "Validación de login";
+    //$user->setUser($userSession->getCurrentUser());
+    //include_once 'vistas/home.php';
+/*}else if(isset($_POST['username']) && isset($_POST['password'])){
+    //echo "Validación de login";*/
 
     $userForm = $_POST['username'];
     $passForm = $_POST['password'];
 
-    if($user->userExists($userForm, $passForm)){
+    if($userForm=='litzy' && $passForm=='holi'){
         //echo "usuario validado";
-        $userSession->setCurrentUser($userForm);
-        $user->setUser($userForm);
+        /*$userSession->setCurrentUser($userForm);
+        $user->setUser($userForm);*/
 
         include_once 'vistas/home.php';
     }else{
